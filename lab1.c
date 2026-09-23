d67768900e2af50c2ca49d4c6d7c0bd460efe6e4
Jerry A Ochoa <jerryochoa8a@aol.com>
2026-09-23 13:34:20 -0700

    lab1 - added initialization(opt1)

--- lab1.c
+++ lab1.c
@@ -1,88 +1,139 @@
 /*-----------------------------------------
 LAB1:  Process Creation Hierarchy
 -------------------------------------------
--To simulate process creation and destruction when 
+-To simulate process creation and destruction when
 implemented with linked lists.
 ==========================================  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h> // for bool
 
 /* Define structures and global constants, including:
 the pcb type, the children linked list type, the maximum number of processes*/
-struct PCB
+
+typedef struct childNode
 {
-   //1) parent: a PCB index corresponding to the process’ creator
+    int index;
+    struct childNode *link;
+} childNode;
 
-   //2) children: a pointer to a linked list, where each node contains the PCB 
-//    index of one child process and a link to the next child in the linked list
-    
-};
-
-struct child
+typedef struct PCB
 {
-    int data;
-    struct child *link;
-};
+    int parent;
+    childNode *children;
+} PCB;
 
+#define MAX_PROCESSES 5
+
+PCB *pcb[MAX_PROCESSES] = {NULL};
 
 /***************************************************************/
 void Print_Hierarchy()
 { //"PROCEDURE TO PRINT HIERARCHY OF PROCESSES"
     /* declare local vars */
+
     /* for each process index i from 0 up to (but not including) maximum number
     of processes*/
-    /* if PCB[i] is not NULL */
-    /* print process id, parent id, list of children processes */
-} /* end of procedure */
+    for (int i = 0; i < MAX_PROCESSES; i++)
+    {
 
+        /* if PCB[i] is not NULL */
+        if (pcb[i] != NULL)
+        {
+
+            /* print process id, parent id, list of children processes */
+            printf("Process id: %d\n", i);
+            if (pcb[i]->parent == -1)
+            {
+                printf("\tNo Parent processes\n");
+            }
+            else
+            {
+                printf("\tParent process: %d\n", pcb[i]->parent);
+            }
+
+            if (pcb[i]->children == NULL)
+            {
+                printf("\tNo child processes\n");
+            }
+            else
+            {
+                printf("\tParent process: %d\n", pcb[i]->children);
+            }
+        }
+    }
+
+} /* end of procedure */
 
 /***************************************************************/
 void option1()
-{ // "PROCEDURE FOR OPTION #1"
+{ // Initialize process hierarchy
     /* declare local vars */
+
     /* Allocate memory for PCB[0] */
+    pcb[0] = malloc(sizeof(PCB));
+    pcb[0]->parent = -1;
+    pcb[0]->children = NULL;
+
     /* Intitialize all other PCB's to NULL */
+    for (int i = 1; i < MAX_PROCESSES; i++)
+    {
+        pcb[i] = NULL;
+    }
+
     /* print hierarchy of processes */
+    Print_Hierarchy();
+
     return;
 } /* end of procedure */
 
-
 /***************************************************************/
+
 void option2()
-{ // "PROCDURE FOR OPTION #2"
+{ // "PROCDURE#2 - Create a new child process
     /* define local vars */
+
     /* prompt for parent process index p */
+    
     /* if PCB[p] is NULL, print message process does not exist, return */
+    
     /* search for first available index q without a parent in a while loop */
+    
     /* if maximum number of processes reached, print message of no more avaiable
     PCBs */
+    
     /* allocate memory for new child process, initialize fields */
+    
     /* record the parent's index p in PCB[q] */
+    
     /* initialize the list of children of PCB[q] as empty */
+    
     /* append the node containing the child's index q to the children linked list
     of PCB[p] */
+    
     /* print hierarchy of processes */
+    Print_Hierarchy();
     return;
 } /* end of procedure */
 
+/***************************************************************/
+
+// void distroyChildNode(struct Node* parameter)
+// { //"RECURSIVE PROCEDURE TO DESTROY CHILDREN PROCESSES"
+//     /* declare local vars */
+//     /* check if end of linked list--if so return */
+//     /* else call self on next node in linked list */
+//     /* set variable q to current node's process index field */
+//     /* call self on children of PCB[q] */
+//     /* free memory of PCB[q] and set PCB[q] to NULL*/
+//     /* free memory of paramter and set to NULL */
+//     return;
+// } /* end of procedure */
 
 /***************************************************************/
-void distroyChildNode(struct Node* parameter)
-{ //"RECURSIVE PROCEDURE TO DESTROY CHILDREN PROCESSES"
-    /* declare local vars */
-    /* check if end of linked list--if so return */
-    /* else call self on next node in linked list */
-    /* set variable q to current node's process index field */
-    /* call self on children of PCB[q] */
-    /* free memory of PCB[q] and set PCB[q] to NULL*/
-    /* free memory of paramter and set to NULL */
-    return;
-} /* end of procedure */
 
-
-/***************************************************************/
 void option3()
-{ //"PROCEDURE FOR OPTION #3"
+{ //"PROCEDURE#3 - Destroy all descendants of a process
     /* declare local vars */
     /* prompt for process index p */
     /* call recursive procedure to destroy children of PCB[p] */
@@ -91,11 +142,11 @@ void option3()
     return;
 } /* end of procedure */
 
-
 /***************************************************************/
+
 void option4()
-{ // "PROCEDURE FOR OPTION #4"
-    /* if PCB[0] is non null)
+{ // "PROCEDURE#4 - Quit program and free memory
+    /* if PCB[0] is non null) */
     /* if children of PCB[0] is not null */
     /* call recursive procedure to destroy children of PCB[0] */
     /* free memory of all PCB's */
@@ -103,12 +154,11 @@ void option4()
 } /* end of procedure */
 /***************************************************************/
 
-
 int main()
 {
     /* declare local vars */
     int userInput;
-    bool userExit=false;
+    bool userExit = false;
 
     /* while user has not chosen to quit */
     while (!userExit)
@@ -133,18 +183,22 @@ int main()
         {
         case 1:
             // Initialize process hierarchy
+            option1();
             break;
 
         case 2:
             // Create a new child process
+            option2();
             break;
 
         case 3:
             // Destroy all descendants of a process
+            option3();
             break;
 
         case 4:
             // Quit program and free memory
+            option4();
             userExit = true;
             break;
 
